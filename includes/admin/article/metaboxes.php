ae6020c35d5450d5a4c1c30540a327b09712005f
Mike Howard <mike@mikeandniki.co.uk>
2016-10-17 18:11:13 +0100

    Added linked tickets metabox

--- includes/admin/article/metaboxes.php
+++ includes/admin/article/metaboxes.php
@@ -44,6 +44,16 @@ function kbs_article_add_meta_boxes( $post )	{
 	}
 
 	add_meta_box(
+		'kbs-article-metabox-linked-tickets',
+		sprintf( __( 'Linked %s', 'kb-support' ), kbs_get_ticket_label_plural() ),
+		'kbs_article_metabox_linked_tickets_callback',
+		'article',
+		'normal',
+		'high',
+		array()
+	);
+
+	add_meta_box(
 		'kbs-article-metabox-options',
 		__( 'Options', 'kb-support' ),
 		'kbs_article_metabox_options_callback',
@@ -57,7 +67,26 @@ function kbs_article_add_meta_boxes( $post )	{
 add_action( 'add_meta_boxes_article', 'kbs_article_add_meta_boxes' );
 
 /**
- * The callback function for the save metabox.
+ * The callback function for the linked tickets metabox.
+ *
+ * @since	1.0
+ * @global	obj		$post				WP_Post object
+ * @global	bool	$kbs_ticket_update	True if this article is being updated, false if new
+ * @return	void
+ */
+function kbs_article_metabox_linked_tickets_callback()	{
+	global $post, $kbs_article_update;
+
+	/*
+	 * Output the items for the linked tickets metabox
+	 * @since	1.0
+	 * @param	int	$post_id	The KB post ID
+	 */
+	do_action( 'kbs_article_linked_tickets_fields', $post->ID );
+} // kbs_article_metabox_linked_tickets_callback
+
+/**
+ * The callback function for the article options metabox.
  *
  * @since	1.0
  * @global	obj		$post				WP_Post object
@@ -78,10 +107,45 @@ function kbs_article_metabox_options_callback()	{
 } // kbs_article_metabox_options_callback
 
 /**
- * Display the KB options metabox fields.
+ * Display the Article Linked Tickets metabox.
  *
  * @since	1.0
- * @global	bool	$kbs_ticket_update	True if this ticket is being updated, false if new.
+ * @global	bool	$kbs_ticket_update	True if this article is being updated, false if new.
+ * @param	int		$post_id			The KB post ID.
+ * @return	str
+ */
+function kbs_article_metabox_linked_tickets_fields( $post_id )	{
+	global $kbs_article_update;
+
+	$linked_tickets = kbs_get_linked_tickets( $post_id );
+
+	if ( ! empty( $linked_tickets ) ) : ?>
+
+    	<?php foreach( $linked_tickets as $linked_ticket ) : ?>
+			<p><a href="#TB_inline?width=600&height=550&inlineId=kbs-ticket-content-<?php echo $linked_ticket; ?>" class="thickbox" title="<?php echo get_the_title( $linked_ticket ); ?>">
+            	# <?php echo kbs_get_ticket_id( $linked_ticket) . '</a> - ' . get_the_title( $linked_ticket ); ?>
+            </p>
+
+			<div id="kbs-ticket-content-<?php echo $linked_ticket; ?>" class="kbs-hidden">
+				<?php do_action( 'kbs_article_before_thickbox_ticket_content', $linked_ticket, $post_id ); ?>
+                <?php echo wpautop( get_post_field( 'post_content', $linked_ticket ) ); ?>
+                <?php do_action( 'kbs_article_after_thickbox_ticket_content', $linked_ticket, $post_id ); ?>
+            </div>
+
+        <?php endforeach; ?>
+
+    <?php else : ?>
+    		<p><?php printf( __( 'There are no %s linked to this article.', 'kb-support' ), kbs_get_ticket_label_plural( true ) ); ?></p>
+    <?php endif;
+
+} // kbs_article_metabox_linked_tickets_fields
+add_action( 'kbs_article_linked_tickets_fields', 'kbs_article_metabox_linked_tickets_fields', 10, 1 );
+
+/**
+ * Display the Article options metabox fields.
+ *
+ * @since	1.0
+ * @global	bool	$kbs_ticket_update	True if this article is being updated, false if new.
  * @param	int		$post_id			The KB post ID.
  * @return	str
  */
