a1cfe89c25beda72b39320e70f87def2e21dddb9
mikeyhoward1977 <mike@mikeandniki.co.uk>
2016-10-18 00:16:39 +0100

    Remove use of extract()

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -25,11 +25,11 @@ if ( ! defined( 'ABSPATH' ) )
  * @return	str
  */
 function kbs_login_form_shortcode( $atts ) {
-	extract( shortcode_atts( array(
+	$args = shortcode_atts( array(
 			'redirect' => '',
-		), $atts, 'kbs_login' )
-	);
-	return kbs_login_form( $redirect );
+	), $atts, 'kbs_login' );
+
+	return kbs_login_form( $args['redirect'] );
 } // kbs_login_form_shortcode
 add_shortcode( 'kbs_login', 'kbs_login_form_shortcode' );
 
@@ -44,11 +44,11 @@ add_shortcode( 'kbs_login', 'kbs_login_form_shortcode' );
  * @return	str
  */
 function kbs_register_form_shortcode( $atts ) {
-	extract( shortcode_atts( array(
-			'redirect' => '',
-		), $atts, 'kbs_register' )
-	);
-	return kbs_register_form( $redirect );
+	$args = shortcode_atts( array(
+		'redirect' => '',
+	), $atts, 'kbs_register' );
+
+	return kbs_register_form( $args['redirect'] );
 } // kbs_register_form_shortcode
 add_shortcode( 'kbs_register', 'kbs_register_form_shortcode' );
 
@@ -126,48 +126,35 @@ function kbs_articles_shortcode( $atts )	{
 		'posts_per_page'  => 20,      // Number of posts to display
 		'orderby'         => 'views', // Order by
 		'order'           => 'DESC',  // Order
-		'tags'            => null,    // Tag IDs to include, comma seperate
-		'categories'      => null,    // Category IDs to include, comma seperate
-		'excerpt'         => true,    // Whether to display an excerpt
+		'tags'            => 0,       // Tag IDs to include, comma seperate
+		'categories'      => 0,       // Category IDs to include, comma seperate
+		'excerpt'         => 1,       // Whether to display an excerpt
 		'length'          => (int) kbs_get_option( 'kbs_article_excerpt_length', 100 ), // Length of excerpt. -1 for full content
-		'hide_restricted' => (bool) kbs_hide_restricted_articles()                      // Whether to hide restricted articles
+		'hide_restricted' => (bool) kbs_hide_restricted_articles() // Whether to hide restricted articles
 	), $atts, 'kbs_articles' );
 
-	$length     = $args['length'];
-	$excerpt    = $args['excerpt'];
-	
-	if ( isset( $args['tags'] ) )	{
+	if ( ! empty( $args['tags'] ) )	{
 		$args['tag__in'] = array( $args['tags'] );
 	}
 
-	if ( isset( $args['categories'] ) )	{
+	if ( ! empty( $args['categories'] ) )	{
 		$args['category__in'] = array( $args['categories'] );
 	}
-
-	unset( $args['length'], $args['excerpt'], $args['tags'], $args['categories'] );
 
 	if ( 'views' == $args['orderby'] )	{
 		$args['meta_key'] = '_kbs_article_views';
 		$args['orderby']  = 'meta_value_num';
 	}
 
-	if ( ! empty( $tags ) )	{
-		$args['tag__in'] = '';
-	}
-
-	if ( ! empty( $tags ) )	{
-		$args['tag__in'] = '';
-	}
-
 	$args['post_type'] = 'article';
 
-	if ( ! $args['hide_restricted'] )	{
+	if ( empty ( $args['hide_restricted'] ) )	{
 		remove_action( 'pre_get_posts', 'kbs_articles_exclude_restricted' );
 	}
 
 	$articles_query = new WP_Query( $args );
 
-	if ( ! $args['hide_restricted'] )	{
+	if ( empty( $args['hide_restricted'] ) )	{
 		add_action( 'pre_get_posts', 'kbs_articles_exclude_restricted' );
 	}
 
@@ -179,11 +166,19 @@ function kbs_articles_shortcode( $atts )	{
     
             <?php while( $articles_query->have_posts() ) :
     
-                $articles_query->the_post(); ?>
+                $articles_query->the_post();
+				$article_id     = get_the_ID(); ?>
     
-                <?php if ( ! $args['hide_restricted'] || kbs_user_can_view_article( get_the_ID() ) ) : ?>
+                <?php if ( ! $args['hide_restricted'] || kbs_user_can_view_article( $article_id ) ) : ?>
     
-                    <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                    <li>
+                    	<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                        <?php if ( ! empty( $args['excerpt'] ) ) : ?>
+                        	<span>
+								<?php echo kbs_get_article_excerpt( $article_id ); ?>
+                            </span>
+                        <?php endif; ?>
+                    </li>
     
                 <?php endif; ?>
     
@@ -194,7 +189,7 @@ function kbs_articles_shortcode( $atts )	{
 
         <?php wp_reset_postdata(); ?>
     <?php else : ?>
-        // no posts found
+        <p><?php printf( __( 'No %s found', 'kb-support' ), kbs_get_articles_label_plural( true ) ); ?></p>
     <?php endif;
 
 	return ob_get_clean();
