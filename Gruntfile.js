3b12ce51137256105f47ecafdfb8bddd59c7339d
mikeyhoward1977 <mike@mikeandniki.co.uk>
2016-11-30 01:11:46 +0000

    Added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+	require('load-grunt-tasks')(grunt);
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages/',    // Where to save the POT file.
+					exclude: ['build/.*'],
+					mainFile: 'kb-support.php',    // Main project file.
+					potFilename: 'kb-support.pot',    // Name of the POT file.
+					potHeaders: {
+						poedit: true,                 // Includes common Poedit headers.
+						'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
+								},
+					type: 'wp-plugin',    // Type of project (wp-plugin or wp-theme).
+					updateTimestamp: true,    // Whether the POT-Creation-Date should be updated without other changes.
+					processPot: function( pot, options ) {
+						pot.headers['report-msgid-bugs-to'] = 'https://kb-support.com/';
+						pot.headers['last-translator'] = 'WP-Translations (http://wp-translations.org/)';
+						pot.headers['language-team'] = 'WP-Translations <wpt@wp-translations.org>';
+						pot.headers['language'] = 'en_US';
+						var translation, // Exclude meta data from pot.
+							excluded_meta = [
+								'Plugin Name of the plugin/theme',
+								'Plugin URI of the plugin/theme',
+								'Author of the plugin/theme',
+								'Author URI of the plugin/theme'
+								];
+									for ( translation in pot.translations[''] ) {
+										if ( 'undefined' !== typeof pot.translations[''][ translation ].comments.extracted ) {
+											if ( excluded_meta.indexOf( pot.translations[''][ translation ].comments.extracted ) >= 0 ) {
+												console.log( 'Excluded meta: ' + pot.translations[''][ translation ].comments.extracted );
+													delete pot.translations[''][ translation ];
+												}
+											}
+										}
+						return pot;
+					}
+				}
+			}
+		}
+	});
+}
