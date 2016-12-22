e3aba426ac6cb8c501ab29d5abc3c2c7fdb99c4d
Mike Howard <mike@mikeandniki.co.uk>
2016-12-22 12:16:08 +0000

    Display notice if no tickets

--- templates/ticket-history.php
+++ templates/ticket-history.php
@@ -10,53 +10,54 @@ if ( is_user_logged_in() )	: ?>
 	$customer = new KBS_Customer( $current_user->ID, true );
 	$tickets  = kbs_get_customer_tickets( $customer->id, array(), false, true ); ?>
 
-	<div id="kbs_item_wrapper" class="kbs_ticket_history_wrapper" style="float: left">
-		<div class="ticket_info_wrapper data_section">
-        	<?php do_action( 'kbs_before_ticket_history_table', $tickets, $customer ); ?>
-        	<table id="ticket_history">
-            	<thead>
-                	<tr id="ticket_history_header">
-                        <th><?php echo '#'; ?></th>
-                        <th><?php _e( 'Opened', 'kb-support' ); ?></th>
-                        <th><?php _e( 'Status', 'kb-support' ); ?></th>
-                        <th><?php _e( 'Actions', 'kb-support' ); ?></th>
-                    </tr>
-                </thead>
+	<?php if ( ! empty( $tickets ) ) : ?>
+        <div id="kbs_item_wrapper" class="kbs_ticket_history_wrapper" style="float: left">
+            <div class="ticket_info_wrapper data_section">
+				<?php do_action( 'kbs_before_ticket_history_table', $tickets, $customer ); ?>
+                <table id="ticket_history">
+                    <thead>
+                        <tr id="ticket_history_header">
+                            <th><?php echo '#'; ?></th>
+                            <th><?php _e( 'Opened', 'kb-support' ); ?></th>
+                            <th><?php _e( 'Status', 'kb-support' ); ?></th>
+                            <th><?php _e( 'Actions', 'kb-support' ); ?></th>
+                        </tr>
+                    </thead>
 
-				<?php if ( ! empty( $tickets ) ) :
+                    <?php foreach ( $tickets as $ticket ) : ?>
 
-					foreach ( $tickets as $ticket ) : ?>
-
-                    	<?php $ticket_url = kbs_get_ticket_url( $ticket->ID ); ?>
-						<tr id="ticket_data_<?php echo $ticket->ID; ?>" class="ticket_data_row">
+                        <?php $ticket_url = kbs_get_ticket_url( $ticket->ID ); ?>
+                        <tr id="ticket_data_<?php echo $ticket->ID; ?>" class="ticket_data_row">
                             <td class="the_ticket_id"><a href="<?php echo $ticket_url; ?>"><?php echo kbs_get_ticket_id( $ticket->ID ); ?></a></td>
                             <td class="date"><?php echo date_i18n( get_option( 'date_format' ), strtotime( $ticket->post_date ) ); ?></td>
                             <td class="status"><?php echo kbs_get_ticket_status( $ticket, true ); ?></td>
                             <td class="actions"><a href="<?php echo $ticket_url; ?>"><?php _e( 'View', 'kb-support' ); ?></a></td>
                         </tr>
 
-					<?php endforeach; ?>
+                    <?php endforeach; ?>
 
-                <?php else : ?>
-                	<td colspan="4" class="no_ticket_notice"><?php printf( __( 'You have no %s yet.', 'kb-support' ), kbs_get_ticket_label_plural( true ) ); ?></td>
-                <?php endif; ?>
+                </table>
+                <?php do_action( 'kbs_after_ticket_history_table', $tickets, $customer ); ?>
 
-            </table>
-            <?php do_action( 'kbs_after_ticket_history_table', $tickets, $customer ); ?>
+            </div>
+    
+            <div id="kbs_ticket_history_pagination" class="kbs_pagination navigation">
+                <?php
+                $big = 999999;
+                echo paginate_links( array(
+                    'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+                    'format'  => '?paged=%#%',
+                    'current' => max( 1, get_query_var( 'paged' ) ),
+                    'total'   => ceil( kbs_get_customer_ticket_count( $customer->id ) / 20 ) // 20 items per page
+                ) );
+                ?>
+            </div>
+
         </div>
 
-        <div id="kbs_ticket_history_pagination" class="kbs_pagination navigation">
-			<?php
-            $big = 999999;
-            echo paginate_links( array(
-                'base'    => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-                'format'  => '?paged=%#%',
-                'current' => max( 1, get_query_var( 'paged' ) ),
-                'total'   => ceil( kbs_get_customer_ticket_count( $customer->id ) / 20 ) // 20 items per page
-            ) );
-            ?>
-        </div>
-    </div>
+	<?php else : ?>
+        <div class="kbs_alert kbs_alert_info"><?php printf( __( 'You have no %s yet.', 'kb-support' ), kbs_get_ticket_label_plural( true ) ); ?></div>
+    <?php endif; ?>
 
 <?php else : ?>
 
