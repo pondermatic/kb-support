1b3aa225ae31afb1f258382ad831d5725f60d80d
mikeyhoward1977 <mike@mikeandniki.co.uk>
2017-02-28 21:07:46 +0000

    Remove scheduled tasks on plugin deactivation

--- uninstall.php
+++ uninstall.php
@@ -101,12 +101,6 @@ if ( kbs_get_option( 'remove_on_uninstall' ) )	{
 	$wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . "kbs_customers" );
 	$wpdb->query( "DROP TABLE IF EXISTS " . $wpdb->prefix . "kbs_customermeta" );
 
-	// Remove Cron Task Hooks
-	wp_clear_scheduled_hook( 'kbs_hourly_scheduled_events' );
-	wp_clear_scheduled_hook( 'kbs_twice_daily_scheduled_events' );
-	wp_clear_scheduled_hook( 'kbs_daily_scheduled_events' );
-	wp_clear_scheduled_hook( 'kbs_weekly_scheduled_events' );
-
 	// Remove any transients and options we've left behind
 	$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '\_transient\_kbs\_%'" );
 	$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '\_transient\_timeout\_kbs\_%'" );
