0cb88002467e7f12af98d5c2191448da3554fe13
mikeyhoward1977 <mike@mikeandniki.co.uk>
2017-08-29 01:06:27 +0100

    Move register article CPT functions to KB class

--- includes/class-kbs-knowledgebase.php
+++ includes/class-kbs-knowledgebase.php
@@ -57,7 +57,8 @@ class KBS_Knowledgebase {
 	 */
 	public function __construct()	{
 		add_action( 'init',            array( $this, 'setup_kb' ) );
-		//add_action( 'kbs_kb_init_kbs', '' );
+		add_action( 'kbs_kb_init_kbs', array( $this, 'register_post_type' ) );
+		add_action( 'kbs_kb_init_kbs', array( $this, 'register_taxonomies' ) );
 	} // __construct
 
 	/**
@@ -101,12 +102,135 @@ class KBS_Knowledgebase {
 	 *
 	 * @since	1.1
 	 */
-	private function get_active_kb()	{
+	public function get_active_kb()	{
 		if ( ! isset( $this->active_kb ) )	{
 			$this->active_kb = kbs_get_option( 'active_kb', 'kbs' );
 		}
 
 		return $this->active_kb;
 	} // get_active_kb
+
+	/**
+	 * Register the default KB Article post type.
+	 *
+	 * @since	1.1
+	 * @return	void
+	 */
+	public function register_post_type()	{
+		$article_archives = defined( 'KBS_ARTICLE_DISABLE_ARCHIVE' ) && KBS_ARTICLE_DISABLE_ARCHIVE ? false : true;
+		$articles_slug    = defined( 'KBS_ARTICLE_SLUG' ) ? KBS_ARTICLE_SLUG : 'articles';
+		$articles_rewrite = defined( 'KBS_ARTICLE_DISABLE_REWRITE' ) && KBS_ARTICLE_DISABLE_REWRITE ? false : array( 'slug' => $articles_slug, 'with_front' => false );
+
+		$article_labels = array(
+			'name'               => _x( '%2$s', 'article type general name', 'kb-support' ),
+			'singular_name'      => _x( '%1$s', 'article type singular name', 'kb-support' ),
+			'add_new'            => __( 'New %1$s', 'kb-support' ),
+			'add_new_item'       => __( 'New %1$s', 'kb-support' ),
+			'edit_item'          => __( 'Edit %1$s', 'kb-support' ),
+			'new_item'           => __( 'New %1$s', 'kb-support' ),
+			'all_items'          => __( '%2$s', 'kb-support' ),
+			'view_item'          => __( 'View %1$s', 'kb-support' ),
+			'search_items'       => __( 'Search %2$s', 'kb-support' ),
+			'not_found'          => __( 'No %2$s found', 'kb-support' ),
+			'not_found_in_trash' => __( 'No %2$s found in Trash', 'kb-support' ),
+			'parent_item_colon'  => '',
+			'menu_name'          => __( '%2$s', 'kb-support' )
+		);
+
+		foreach ( $article_labels as $key => $value ) {
+			$article_labels[ $key ] = sprintf( $value, kbs_get_article_label_singular(), kbs_get_article_label_plural() );
+		}
+
+		$article_args = array(
+			'labels'             => $article_labels,
+			'public'             => true,
+			'show_in_menu'       => true,
+			'menu_icon'          => 'dashicons-welcome-learn-more',
+			'query_var'          => true,
+			'rewrite'            => $articles_rewrite,
+			'capability_type'    => 'article',
+			'map_meta_cap'       => true,
+			'has_archive'        => $article_archives,
+			'hierarchical'       => false,
+			'supports'           => apply_filters( 'kbs_article_supports', array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author', 'trackbacks', 'comments' ) ),
+			'can_export'         => true
+		);
+
+		register_post_type( 'article', $article_args );
+	} // register_post_type
+
+	/**
+	 * Register the default KB Article taxonomies.
+	 *
+	 * @since	1.1
+	 * @return	void
+	 */
+	public function register_taxonomies()	{
+		$articles_slug = defined( 'KBS_ARTICLE_SLUG' ) ? KBS_ARTICLE_SLUG : 'articles';
+
+		$article_category_labels = array(
+			'name'              => _x( 'Categories', 'taxonomy general name', 'kb-support' ),
+			'singular_name'     => _x( 'Category', 'taxonomy singular name', 'kb-support' ),
+			'search_items'      => sprintf( __( 'Search %s Categories', 'kb-support' ), kbs_get_article_label_singular() ),
+			'all_items'         => sprintf( __( 'All %s Categories', 'kb-support' ), kbs_get_article_label_singular() ),
+			'parent_item'       => sprintf( __( 'Parent %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
+			'parent_item_colon' => sprintf( __( 'Parent %s Category:', 'kb-support' ), kbs_get_article_label_singular() ),
+			'edit_item'         => sprintf( __( 'Edit %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
+			'update_item'       => sprintf( __( 'Update %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
+			'add_new_item'      => sprintf( __( 'Add New %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
+			'new_item_name'     => sprintf( __( 'New %s Category Name', 'kb-support' ), kbs_get_article_label_singular() ),
+			'menu_name'         => __( 'Categories', 'kb-support' )
+		);
+
+		$article_category_args = apply_filters( 'kbs_article_category_args', array(
+			'hierarchical' => true,
+			'labels'       => apply_filters( 'kbs_article_category_labels', $article_category_labels ),
+			'show_ui'      => true,
+			'query_var'    => 'article_category',
+			'rewrite'      => array( 'slug' => $articles_slug . '/category', 'with_front' => false, 'hierarchical' => true ),
+			'capabilities' => array(
+				'manage_terms' => 'manage_article_terms',
+				'edit_terms'   => 'edit_article_terms',
+				'assign_terms' => 'assign_article_terms',
+				'delete_terms' => 'delete_article_terms'
+			)
+		) );
+
+		register_taxonomy( 'article_category', array( 'article' ), $article_category_args );
+		register_taxonomy_for_object_type( 'article_category', 'article' );
+
+		/** Article Tags */
+		$article_tag_labels = array(
+			'name'                  => _x( 'Tags', 'taxonomy general name', 'kb-support' ),
+			'singular_name'         => _x( 'Tag', 'taxonomy singular name', 'kb-support' ),
+			'search_items'          => sprintf( __( 'Search %s Tags', 'kb-support' ), kbs_get_article_label_singular() ),
+			'all_items'             => sprintf( __( 'All %s Tags', 'kb-support' ), kbs_get_article_label_singular() ),
+			'parent_item'           => sprintf( __( 'Parent %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
+			'parent_item_colon'     => sprintf( __( 'Parent %s Tag:', 'kb-support' ), kbs_get_article_label_singular() ),
+			'edit_item'             => sprintf( __( 'Edit %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
+			'update_item'           => sprintf( __( 'Update %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
+			'add_new_item'          => sprintf( __( 'Add New %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
+			'new_item_name'         => sprintf( __( 'New %s Tag Name', 'kb-support' ), kbs_get_article_label_singular() ),
+			'menu_name'             => __( 'Tags', 'kb-support' ),
+			'choose_from_most_used' => sprintf( __( 'Choose from most used %s tags', 'kb-support' ), kbs_get_article_label_singular() )
+		);
+
+		$article_tag_args = apply_filters( 'kbs_article_tag_args', array(
+			'hierarchical' => false,
+			'labels'       => apply_filters( 'kbs_article_tag_labels', $article_tag_labels ),
+			'show_ui'      => true,
+			'query_var'    => 'article_tag',
+			'rewrite'      => array( 'slug' => $articles_slug . '/tag', 'with_front' => false, 'hierarchical' => true  ),
+			'capabilities' => array(
+				'manage_terms' => 'manage_article_terms',
+				'edit_terms'   => 'edit_article_terms',
+				'assign_terms' => 'assign_article_terms',
+				'delete_terms' => 'delete_article_terms'
+			)
+		) );
+
+		register_taxonomy( 'article_tag', array( 'article' ), $article_tag_args );
+		register_taxonomy_for_object_type( 'article_tag', 'article' );
+	} // register_taxonomies
 
 } // KBS_Knowledgebase
--- includes/post-types.php
+++ includes/post-types.php
@@ -20,10 +20,6 @@ if ( ! defined( 'ABSPATH' ) )
  * @return	void
  */
 function kbs_setup_post_types() {
-	
-	$article_archives = defined( 'KBS_ARTICLE_DISABLE_ARCHIVE' ) && KBS_ARTICLE_DISABLE_ARCHIVE ? false : true;
-	$articles_slug    = defined( 'KBS_ARTICLE_SLUG' ) ? KBS_ARTICLE_SLUG : 'articles';
-	$articles_rewrite = defined( 'KBS_ARTICLE_DISABLE_REWRITE' ) && KBS_ARTICLE_DISABLE_REWRITE ? false : array( 'slug' => $articles_slug, 'with_front' => false );
 
 	$ticket_labels =  apply_filters( 'kbs_ticket_labels', array(
 		'name'                  => _x( '%2$s', 'kbs_ticket post type name', 'kb-support' ),
@@ -104,48 +100,6 @@ function kbs_setup_post_types() {
 	);
 
 	register_post_type( 'kbs_ticket_reply', apply_filters( 'kbs_ticket_reply_post_type_args', $ticket_reply_args ) );
-
-	if ( 'article' == KBS()->KB->post_type )	{
-
-		/** Article Post Type */
-		$article_labels = array(
-			'name'               => _x( '%2$s', 'article type general name', 'kb-support' ),
-			'singular_name'      => _x( '%1$s', 'article type singular name', 'kb-support' ),
-			'add_new'            => __( 'New %1$s', 'kb-support' ),
-			'add_new_item'       => __( 'New %1$s', 'kb-support' ),
-			'edit_item'          => __( 'Edit %1$s', 'kb-support' ),
-			'new_item'           => __( 'New %1$s', 'kb-support' ),
-			'all_items'          => __( '%2$s', 'kb-support' ),
-			'view_item'          => __( 'View %1$s', 'kb-support' ),
-			'search_items'       => __( 'Search %2$s', 'kb-support' ),
-			'not_found'          => __( 'No %2$s found', 'kb-support' ),
-			'not_found_in_trash' => __( 'No %2$s found in Trash', 'kb-support' ),
-			'parent_item_colon'  => '',
-			'menu_name'          => __( '%2$s', 'kb-support' )
-		);
-
-		foreach ( $article_labels as $key => $value ) {
-			$article_labels[ $key ] = sprintf( $value, kbs_get_article_label_singular(), kbs_get_article_label_plural() );
-		}
-
-		$article_args = array(
-			'labels'             => $article_labels,
-			'public'             => true,
-			'show_in_menu'       => true,
-			'menu_icon'          => 'dashicons-welcome-learn-more',
-			'query_var'          => true,
-			'rewrite'            => $articles_rewrite,
-			'capability_type'    => 'article',
-			'map_meta_cap'       => true,
-			'has_archive'        => $article_archives,
-			'hierarchical'       => false,
-			'supports'           => apply_filters( 'kbs_article_supports', array( 'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'author', 'trackbacks', 'comments' ) ),
-			'can_export'         => true
-		);
-
-		register_post_type( 'article', $article_args );
-
-	}
 	
 	/** KB Form Type */
 	$form_labels = array(
@@ -373,8 +327,6 @@ add_filter( 'enter_title_here', 'kbs_change_default_title' );
 */
 function kbs_setup_custom_taxonomies() {
 
-	$articles_slug = defined( 'KBS_ARTICLE_SLUG' ) ? KBS_ARTICLE_SLUG : 'articles';
-
 	/** Ticket Categories */
 	$ticket_category_labels = array(
 		'name'              => _x( 'Categories', 'taxonomy general name', 'kb-support' ),
@@ -448,72 +400,6 @@ function kbs_setup_custom_taxonomies() {
 	register_taxonomy( 'ticket_tag', array( 'kbs_ticket' ), $ticket_tag_args );
 	register_taxonomy_for_object_type( 'ticket_tag', 'kbs_ticket' );
 
-	/** Article Categories */
-	$article_category_labels = array(
-		'name'              => _x( 'Categories', 'taxonomy general name', 'kb-support' ),
-		'singular_name'     => _x( 'Category', 'taxonomy singular name', 'kb-support' ),
-		'search_items'      => sprintf( __( 'Search %s Categories', 'kb-support' ), kbs_get_article_label_singular() ),
-		'all_items'         => sprintf( __( 'All %s Categories', 'kb-support' ), kbs_get_article_label_singular() ),
-		'parent_item'       => sprintf( __( 'Parent %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
-		'parent_item_colon' => sprintf( __( 'Parent %s Category:', 'kb-support' ), kbs_get_article_label_singular() ),
-		'edit_item'         => sprintf( __( 'Edit %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
-		'update_item'       => sprintf( __( 'Update %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
-		'add_new_item'      => sprintf( __( 'Add New %s Category', 'kb-support' ), kbs_get_article_label_singular() ),
-		'new_item_name'     => sprintf( __( 'New %s Category Name', 'kb-support' ), kbs_get_article_label_singular() ),
-		'menu_name'         => __( 'Categories', 'kb-support' )
-	);
-
-	$article_category_args = apply_filters( 'kbs_article_category_args', array(
-			'hierarchical' => true,
-			'labels'       => apply_filters( 'kbs_article_category_labels', $article_category_labels ),
-			'show_ui'      => true,
-			'query_var'    => 'article_category',
-			'rewrite'      => array( 'slug' => $articles_slug . '/category', 'with_front' => false, 'hierarchical' => true ),
-			'capabilities' => array(
-				'manage_terms' => 'manage_article_terms',
-				'edit_terms'   => 'edit_article_terms',
-				'assign_terms' => 'assign_article_terms',
-				'delete_terms' => 'delete_article_terms'
-			)
-		)
-	);
-
-	register_taxonomy( 'article_category', array( 'article' ), $article_category_args );
-	register_taxonomy_for_object_type( 'article_category', 'article' );
-
-	/** Article Tags */
-	$article_tag_labels = array(
-		'name'                  => _x( 'Tags', 'taxonomy general name', 'kb-support' ),
-		'singular_name'         => _x( 'Tag', 'taxonomy singular name', 'kb-support' ),
-		'search_items'          => sprintf( __( 'Search %s Tags', 'kb-support' ), kbs_get_article_label_singular() ),
-		'all_items'             => sprintf( __( 'All %s Tags', 'kb-support' ), kbs_get_article_label_singular() ),
-		'parent_item'           => sprintf( __( 'Parent %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
-		'parent_item_colon'     => sprintf( __( 'Parent %s Tag:', 'kb-support' ), kbs_get_article_label_singular() ),
-		'edit_item'             => sprintf( __( 'Edit %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
-		'update_item'           => sprintf( __( 'Update %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
-		'add_new_item'          => sprintf( __( 'Add New %s Tag', 'kb-support' ), kbs_get_article_label_singular() ),
-		'new_item_name'         => sprintf( __( 'New %s Tag Name', 'kb-support' ), kbs_get_article_label_singular() ),
-		'menu_name'             => __( 'Tags', 'kb-support' ),
-		'choose_from_most_used' => sprintf( __( 'Choose from most used %s tags', 'kb-support' ), kbs_get_article_label_singular() )
-	);
-
-	$article_tag_args = apply_filters( 'kbs_article_tag_args', array(
-			'hierarchical' => false,
-			'labels'       => apply_filters( 'kbs_article_tag_labels', $article_tag_labels ),
-			'show_ui'      => true,
-			'query_var'    => 'article_tag',
-			'rewrite'      => array( 'slug' => $articles_slug . '/tag', 'with_front' => false, 'hierarchical' => true  ),
-			'capabilities' => array(
-				'manage_terms' => 'manage_article_terms',
-				'edit_terms'   => 'edit_article_terms',
-				'assign_terms' => 'assign_article_terms',
-				'delete_terms' => 'delete_article_terms'
-			)
-		)
-	);
-
-	register_taxonomy( 'article_tag', array( 'article' ), $article_tag_args );
-	register_taxonomy_for_object_type( 'article_tag', 'article' );
 
 } // kbs_setup_custom_taxonomies
 add_action( 'init', 'kbs_setup_custom_taxonomies', 0 );
@@ -655,15 +541,17 @@ function kbs_updated_messages( $messages ) {
 		7 => sprintf( __( '%1$s saved.', 'kb-support'     ), $url2 ),
 		8 => sprintf( __( '%1$s submitted.', 'kb-support' ), $url2 )
 	);
-	
-	$messages['article'] = array(
-		1  => sprintf( __( '%2$s updated. %1$sView %2$s%3$s.', 'kb-support'   ), $url1, $url3, $url4 ),
-		4  => sprintf( __( '%2$s updated. %1$sView %2$s%3$s.', 'kb-support'   ), $url1, $url3, $url4 ),
-		6  => sprintf( __( '%2$s published. %1$sView %2$s%3$s.', 'kb-support' ), $url1, $url3, $url4 ),
-		7  => sprintf( __( '%2$s saved. %1$sView %2$s%3$s.', 'kb-support'     ), $url1, $url3, $url4 ),
-		8  => sprintf( __( '%2$s submitted. %1$sView %2$s%3$s.', 'kb-support' ), $url1, $url3, $url4 ),
-		10 => sprintf( __( '%1$s draft updated. <a target="_blank" href="%2$s">Preview %1$s</a>', 'kb-support' ), $url3, esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) )
-	);
+
+	if ( KBS()->KB->default_kb )	{
+		$messages['article'] = array(
+			1  => sprintf( __( '%2$s updated. %1$sView %2$s%3$s.', 'kb-support'   ), $url1, $url3, $url4 ),
+			4  => sprintf( __( '%2$s updated. %1$sView %2$s%3$s.', 'kb-support'   ), $url1, $url3, $url4 ),
+			6  => sprintf( __( '%2$s published. %1$sView %2$s%3$s.', 'kb-support' ), $url1, $url3, $url4 ),
+			7  => sprintf( __( '%2$s saved. %1$sView %2$s%3$s.', 'kb-support'     ), $url1, $url3, $url4 ),
+			8  => sprintf( __( '%2$s submitted. %1$sView %2$s%3$s.', 'kb-support' ), $url1, $url3, $url4 ),
+			10 => sprintf( __( '%1$s draft updated. <a target="_blank" href="%2$s">Preview %1$s</a>', 'kb-support' ), $url3, esc_url( add_query_arg( 'preview', 'true', get_permalink( $post_ID ) ) ) )
+		);
+	}
 	
 	$messages['kbs_form'] = array(
 		1  => __( 'Form updated.', 'kb-support'   ),
