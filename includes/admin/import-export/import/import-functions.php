7ff9bf49be265a7dbe467943ae3d7f056a1d0b0a
mikeyhoward1977 <mike@mikeandniki.co.uk>
2017-09-01 22:10:43 +0100

    Split import and export function files

---
+++ includes/admin/import-export/import/import-functions.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Import Functions
+ *
+ * These are functions are used for importing data into KBS.
+ *
+ * @package     KBS
+ * @subpackage  Admin/Import
+ * @copyright   Copyright (c) 2016, Mike Howard
+ * @since       1.1
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+require_once( KBS_PLUGIN_DIR . '/includes/admin/import-export/import/import-actions.php' );
--- kb-support.php
+++ kb-support.php
@@ -295,6 +295,7 @@ final class KB_Support {
 			require_once KBS_PLUGIN_DIR . 'includes/admin/settings/contextual-help.php';
 			require_once KBS_PLUGIN_DIR . 'includes/admin/thickbox.php';
 			require_once KBS_PLUGIN_DIR . 'includes/admin/tools.php';
+			require_once KBS_PLUGIN_DIR . 'includes/admin/import-export/import/import-functions.php';
             require_once KBS_PLUGIN_DIR . 'includes/admin/import-export/export/export-functions.php';
 			require_once KBS_PLUGIN_DIR . 'includes/admin/extensions.php';
 			require_once KBS_PLUGIN_DIR . 'includes/admin/upgrades/upgrade-functions.php';
