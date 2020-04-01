b5ea7b5c83d26616a83f851d8997503d48b8efea
mikeyhoward1977 <mike@mikeandniki.co.uk>
2020-04-01 19:46:57 +0100

    Only display closure notice on tickets page

--- assets/js/admin-scripts.js
+++ assets/js/admin-scripts.js
@@ -191,7 +191,7 @@ jQuery(document).ready(function ($) {
             });
 
             $( document.body ).on( 'change', '#ticket_status', function() {
-                if ( 'closed' === $(this).val() && ! kbs_vars.disable_closure_email ) {
+                if ( 'kbs_ticket' === kbs_vars.post_type && 'closed' === $(this).val() && ! kbs_vars.disable_closure_email ) {
                     $(this).parent().append('<br id="kbs-closure-option">');
                     $(this).parent().append('<input type="checkbox" id="kbs-closure-email" name="kbs_closure_email" value="1" style="margin-top:0; margin-left: 4px;">');
                     $(this).parent().append('<label for="kbs-closure-email">' + kbs_vars.send_closure_email + '</label>');
