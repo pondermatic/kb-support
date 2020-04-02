c895cfc79e0d75a09c97ad8e9da60c4fae50fe75
Mike Howard <mike@the-howards.co.uk>
2020-04-02 12:51:43 +0100

    Re-implement agent count from user meta

--- includes/agents/class-kbs-agent.php
+++ includes/agents/class-kbs-agent.php
@@ -243,8 +243,8 @@ class KBS_Agent {
      * @return  bool    Open ticket count
      */
     public function count_open_tickets()    {
-        //$count = get_user_option( 'kbs_open_tickets', $this->id );
-        $count = false;
+        $count = get_user_option( 'kbs_open_tickets', $this->id );
+        //$count = false;
 
 		if ( false === $count ) {
 			$count = count( $this->get_open_tickets() );
