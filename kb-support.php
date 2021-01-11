6ceadbed2892efc4867ec75d747c675fa43efa04
mikeyhoward1977 <mike@the-howards.co.uk>
2021-01-11 21:00:56 +0000

    Register meta for companies and replies API

--- kb-support.php
+++ kb-support.php
@@ -234,6 +234,7 @@ final class KB_Support {
 		require_once KBS_PLUGIN_DIR . 'includes/class-kbs-cache-helper.php';
 		require_once KBS_PLUGIN_DIR . 'includes/post-types.php';
 		require_once KBS_PLUGIN_DIR . 'includes/post-taxonomies.php';
+        require_once KBS_PLUGIN_DIR . 'includes/post-meta.php';
 		require_once KBS_PLUGIN_DIR . 'includes/class-kbs-db.php';
 		require_once KBS_PLUGIN_DIR . 'includes/class-kbs-stats.php';
 		require_once KBS_PLUGIN_DIR . 'includes/class-kbs-roles.php';
