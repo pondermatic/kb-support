ef4f6700c3eb2fc8f0a144135abb567fb7a05d27
Razvan Aldea <44024114+razvanaldea89@users.noreply.github.com>
2023-08-25 17:29:15 +0300

    indentation; phpdocs

--- includes/admin/upsells/class-kbs-admin-upsells.php
+++ includes/admin/upsells/class-kbs-admin-upsells.php
@@ -1,33 +1,38 @@
 <?php
 
-class KBS_Admin_Upsells	{
+class KBS_Admin_Upsells {
 
 	/**
-	 * Get things going.
+	 * Upsell class.
+	 *
+	 * @since 1.5.90
 	 */
-	public function __construct()	{
+	public function __construct() {
 
-		// Upgrade to PRO plugin action link
+		// Upgrade to PRO plugin action link.
 		add_filter( 'kbs_admin_pages', array( $this, 'include_admin_style' ), 60 );
-        add_filter( 'plugin_action_links_' . plugin_basename( KBS_PLUGIN_FILE ), array( $this, 'filter_action_links' ), 60 );
-        add_action( 'admin_menu', array( $this, 'add_lite_vs_pro_page' ), 120 );
+		add_filter( 'plugin_action_links_' . plugin_basename( KBS_PLUGIN_FILE ), array(
+			$this,
+			'filter_action_links'
+		),          60 );
+		add_action( 'admin_menu', array( $this, 'add_lite_vs_pro_page' ), 120 );
 
 	}
 
-    /**
+	/**
 	 * Add the Upgrade to PRO plugin action link
 	 *
 	 * @param $links
 	 *
 	 * @return array
 	 *
-	 * @since 1.5.91
+	 * @since 1.5.90
 	 */
 	public function filter_action_links( $links ) {
 
 		$upgrade = apply_filters( 'kb_support_upgrade_plugin_action', array(
-				'upgrade_available' => true,
-				'link'              => '<a  target="_blank" class="kbs-upgrate-to-pro" href="https://kb-support.com/pricing/?utm_source=kb-support&utm_medium=plugin_settings&utm_campaign=upsell">' . esc_html__( 'Upgrade to PRO!', 'kb-support' ) . '</a>'
+			'upgrade_available' => true,
+			'link'              => '<a  target="_blank" class="kbs-upgrate-to-pro" href="https://kb-support.com/pricing/?utm_source=kb-support&utm_medium=plugin_settings&utm_campaign=upsell">' . esc_html__( 'Upgrade to PRO!', 'kb-support' ) . '</a>'
 		) );
 
 		if ( ! $upgrade['upgrade_available'] ) {
@@ -59,18 +64,25 @@ class KBS_Admin_Upsells	{
 	 * Add lite vs pro page in menu
 	 *
 	 * @param [type] $links
+	 *
 	 * @return void
+	 * @since 1.5.90
 	 */
 	public function add_lite_vs_pro_page() {
 
-        add_submenu_page( 'edit.php?post_type=kbs_ticket', __( 'LITE vs Premium', 'kb-support' ), __( 'LITE vs Premium', 'kb-support' ), 'manage_options', 'kbs-lite-vs-pro', array( $this, 'lits_vs_pro_page' ), 200 );
+		add_submenu_page( 'edit.php?post_type=kbs_ticket', __( 'LITE vs Premium', 'kb-support' ), __( 'LITE vs Premium', 'kb-support' ), 'manage_options', 'kbs-lite-vs-pro', array(
+			$this,
+			'lits_vs_pro_page'
+		),                200 );
 
 	}
 
-    /**
+	/**
 	 * The LITE vs PRO page
 	 *
 	 * @return void
+	 *
+	 * @since 1.5.90
 	 */
 	public function lits_vs_pro_page() {
 
